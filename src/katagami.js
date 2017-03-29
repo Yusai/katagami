d498b976610509e35fb1e60ae859596e82600bd6
Yusai <yusai-works@outlook.com>
2017-03-29 15:55:03 +0900

    'modified'

--- src/katagami.js
+++ src/katagami.js
@@ -1,1 +1,27 @@
 console.log('start app');
+
+var iconContainer = document.getElementById('iconContainer');
+
+//https://developer.github.com/v3/
+function foo(response) {
+    console.log(response);
+    function* generator() {
+        yield* response.data;
+    }
+    var files = generator();
+    function loadSVG() {
+        var tmp = files.next();
+        if (tmp.done) return;
+        //
+        var fileName = "https://yusai.github.io/katagami/" + tmp.value.path;
+        var img = document.createElement('img');
+        img.onload = function() {
+            var li = document.createElement('li');
+            li.appendChild(img);
+            iconContainer.appendChild(li);
+            loadSVG();
+        };
+        img.src = fileName;
+    }
+    loadSVG();
+}
